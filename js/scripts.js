0860994eb05a8f8ceda9ea3a4c8512aa4c236448
Nicholas Jensen <nicholasjensenhay@gmail.com>
2016-01-29 08:18:18 -0800

    define prototype costOfPizza for PizzaMenu

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 function PizzaMenu() {
-  this.sizes = ['Personal', 'Small', 'Medium', 'Large', 'Family'];
-  this.toppings = ['Pepperoni'];
+  this.sizes = ['Personal 8"', 'Small 10"', 'Medium 12"', 'Large 14"', 'Family 16"'];
+  this.costPerSize = [ 8.99, 10.99, 12.99, 14.99, 16.99 ];
+  this.toppings = ['Pepperoni', 'Beef',  'Black Olives',   'Canadian Bacon',  'Crispy Bacon',   'Garlic',   'Green Peppers',  'Grilled Chicken',   'Herb & Cheese Blend',   'Italian Sausage',  'Artichoke Hearts',   'Mixed Onions',   'Mozzarella Cheese',  'Mushrooms',  ' Onions',   'Parmesan Cheese',    'Pineapple',   'Roma Tomatoes',  'Salami',   'Spinach',   'Sun-Dried Tomatoes',  'Zucchini',   'Extra Cheese'];
+  this.costPerTopping = 1;
+  this.firstToppingsIncluded = 2;
+}
+
+PizzaMenu.prototype.costOfPizza = function(size, numOfToppings) {
+  var cost = this.costPerSize[this.sizes.indexOf(size)];
+  if(numOfToppings > this.firstToppingsIncluded) {
+    cost += (numOfToppings - this.firstToppingsIncluded) * this.costPerTopping;
+  }
+  return cost;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -7,4 +7,12 @@ describe("PizzaMenu", function() {
     var menu = new PizzaMenu();
     expect(menu.toppings[0]).to.equal('Pepperoni');
   });
+  it('tells you the cost of a pizza given size and number of toppings', function() {
+    var menu = new PizzaMenu();
+    expect(menu.costOfPizza('Personal 8"', 3)).to.equal(9.99);
+  });
+  it('provides a list of toppings in divs for display', function() {
+    var menu = new PizzaMenu();
+    expect(menu.getToppings()).to.equal();
+  })
 });
